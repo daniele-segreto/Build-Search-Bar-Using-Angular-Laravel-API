40ef070901ed675beeb50235ed89689baecb8d23
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-04-15 15:27:20 +0200

    sistemato il campo descriprion della tabella products, per evitare l'errore relativo alla lunghezza massima

--- backend/database/migrations/2024_04_15_104542_products.php
+++ backend/database/migrations/2024_04_15_104542_products.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->string('description', 500)->nullable(); // sistemato per evitare l'errore relativo alla lunghezza massima
             $table->string('image');
             $table->integer('price');
             $table->timestamps();
