e5e5fa35a208022669f3339a25b692c4eb66321d
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-04-15 16:41:25 +0200

    inserita la funzione per recuperare i prodotti in ProductController.php

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function products()
+    {
+        // Recupera tutti i prodotti dal database utilizzando il modello Product e assegnali alla variabile $products.
+        $products = Product::all();
+
+        // Restituisce la collezione di prodotti.
+        return $products;
+    }
 }
