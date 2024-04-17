56b08cb9ed9d7b8b5529d4918600a0da3da8bd24
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-04-17 14:40:59 +0200

    creato e impostato il file environment

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,10 @@
+// Questo è il file di configurazione dell'ambiente.
+// Contiene le impostazioni per l'ambiente di sviluppo.
+
+// Definizione dell'oggetto environment che esportiamo.
+export const environment = {
+  // Impostazione di produzione su false durante lo sviluppo.
+  production: false,
+  // Definizione dell'URL dell'API utilizzato durante lo sviluppo locale.
+  apiUrl: 'http://127.0.0.1/api', // Modifica questo URL se necessario per l'ambiente di sviluppo.
+};
