7e4cdf9ac2c95c771c9bb8f3bde8dede05839b31
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-04-17 11:06:35 +0200

    correzione parametro sulla funzione precedente

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
-  getproductsSearch() {
+  getproductsSearch(name : any) {
 
   }
 }
