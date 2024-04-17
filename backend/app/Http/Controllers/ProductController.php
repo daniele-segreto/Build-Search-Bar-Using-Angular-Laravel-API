47464d14c6d8273bda618f1090a431598683d17a
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-04-17 09:29:06 +0200

    creata la funzione searchProduct() nel ProductController (per fare la ricerca dei prodotti)

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -16,4 +16,24 @@ class ProductController extends Controller
         // Restituisce la collezione di prodotti.
         return $products;
     }
+
+    public function searchProduct(Request $request)
+    {
+        // Inizializza una nuova query per il modello Product.
+        $query = Product::query();
+
+        // Ottieni i dati di ricerca dalla richiesta.
+        $data = $request->input('search_product');
+
+        // Se sono presenti dati di ricerca...
+        if ($data) {
+            // Aggiungi una clausola WHERE alla query per cercare il titolo del prodotto che contiene la stringa di ricerca.
+            // Aggiungi anche una clausola OR per cercare nella descrizione del prodotto che contiene la stringa di ricerca.
+            $query->whereRaw("title LIKE '%" . $data . "%'")
+                ->orWhereRaw("description LIKE '%" . $data . "%'");
+        }
+
+        // Esegui la query e restituisci i risultati.
+        return $query->get();
+    }
 }
