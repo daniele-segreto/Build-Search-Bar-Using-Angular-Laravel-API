03740f48be86f0b9f9252085292c79b3e6761e3c
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-04-17 12:41:32 +0200

    ottengo il valore dell'input dell'utente e lo stampo in console attraverso la funzione getproductsSearch()

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
-  getproductsSearch(name : any) {
+export class SearchComponent implements OnInit {
 
+  constructor() { };
+
+  ngOnInit(): void {
   }
+
+  getproductsSearch(name: any) {
+      const keyword = name.target.value; // Ottiene il valore dell'input dell'utente
+      console.log(keyword); // Stampa il valore dell'input nella console
+  }
+
 }
