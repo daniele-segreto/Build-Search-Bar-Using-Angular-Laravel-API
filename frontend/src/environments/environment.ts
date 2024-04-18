13e7ba2096070ff8203eb3be0dca6be225aab89c
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-04-18 15:07:32 +0200

    correta apiUrl sul file environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,5 +6,5 @@ export const environment = {
   // Impostazione di produzione su false durante lo sviluppo.
   production: false,
   // Definizione dell'URL dell'API utilizzato durante lo sviluppo locale.
-  apiUrl: 'http://127.0.0.1/api', // Modifica questo URL se necessario per l'ambiente di sviluppo.
+  apiUrl: 'http://127.0.0.1:8000/api', // Modifica questo URL se necessario per l'ambiente di sviluppo.
 };
