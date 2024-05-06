cf9be3a927d44b78c2ac782047671b60034dd4b0
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-05-06 17:23:33 +0200

    creata la promise per la gestione asincrona della richiesta http su data.service

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getSearchProductName(name: string) {
+    const response = new Promise(resolve => { // Crea una nuova Promise per la gestione asincrona della richiesta HTTP
+      this.http.get(environment.apiUrl + `/products/search_product?search_product=${name}`).subscribe(data => { // Effettua una richiesta GET all'API con il parametro di ricerca
+        resolve(data); // Risolve la Promise con i dati ottenuti dalla richiesta
+      }, err => { // Gestisce eventuali errori nella richiesta
+        console.log(err); // Stampa l'errore nella console
+      });
+    });
+    return response; // Restituisce la Promise
+  }
 }
