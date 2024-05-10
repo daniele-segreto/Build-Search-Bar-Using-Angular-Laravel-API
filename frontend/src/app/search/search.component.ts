a6e2fbd3886533bc45037833fec2b56eff502842
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-05-10 17:17:52 +0200

    chiamato il metodo del servizio per effettuare la ricerca (serch.component.ts)

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-search',
@@ -6,15 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  data: any;
 
-  constructor() { };
+  constructor(private dataService: DataService) { };
 
   ngOnInit(): void {
   }
 
   getproductsSearch(name: any) {
-      const keyword = name.target.value; // Ottiene il valore dell'input dell'utente
-      console.log(keyword); // Stampa il valore dell'input nella console
+    const keyword = name.target.value; // Ottiene il valore dell'input dell'utente
+    // console.log(keyword); // Commentato, quindi non verrà stampato il valore dell'input nella console
+
+    const search = this.dataService.getSearchProductName(keyword).then(response => { // Chiama il metodo del servizio per effettuare la ricerca
+      this.data = response; // Assegna i dati ottenuti dalla ricerca alla variabile 'data'
+      console.log(this.data); // Stampa i dati ottenuti dalla ricerca nella console
+    })
   }
 
 }
